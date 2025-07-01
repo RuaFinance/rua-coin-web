996d4665417b0fe09ecc9c205fef3a3ec7055ff1
chenjjiaa <94773058+chenjjiaa@users.noreply.github.com>
2025-07-01 12:01:00 +0800

    chore(ui): update 404 not found page.

--- cex/src/pages/NotFoundPage.jsx
+++ cex/src/pages/NotFoundPage.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => {
+
   return (
-    <div className="container mx-auto p-4 text-center">
-      <div className="py-16">
-        <h1 className="text-4xl font-bold text-white mb-4">404</h1>
-        <p className="text-xl text-gray-400 mb-8">页面未找到</p>
-        <Link to="/" className="btn-primary px-6 py-3">
-          返回首页
-        </Link>
+    <div className="bg-[#f1f2f3]" >
+    <div className="mx-auto text-center error-container">
+      <div className="h-[30px] bg-[#f1f2f3]"></div>
+      <div className="bg-white" >
+        <img src="/asserts/very_sorry.png" className="mx-auto" alt="Not Found" />
+        <div className="py-0" style={{padding: "0 0 40px 0"}}>
+          <Link to="/" 
+            className="rollback-btn font-bold"
+          >
+            返回到首页
+          </Link>
+        </div>
+
+        <div className="error-split" id="up"></div>
+
+        <div className='error-manga'>
+          <img src="/asserts/HtPXYfpuXU.png" className="mx-auto" />
+        </div>
       </div>
+    </div>
     </div>
   );
 };
